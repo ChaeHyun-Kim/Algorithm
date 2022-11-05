e89d55e3056ea08fadc430582e099d6d82c1fa27
Re-Kor <chyun1750@gmail.com>
2022-11-05 18:16:33 +0900

    level0/003

---
+++ level0/003.js
@@ -0,0 +1,12 @@
+// 숫자 비교하기
+function solution(num1, num2) {
+  return num1 === num2 ? 1 : -1;
+}
+
+// 런타임 에러! 
+// 삼항연산자 중괄호 필요없음!
+function solution(num1, num2) {
+    let answer = {num1 === num2 ? 1 : -1};
+     return answer
+  }
+  
